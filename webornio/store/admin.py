26245962087ede6d6159004f9ca32e06554bcba5
Henrik Hartiala <hartialh@student.tut.fi>
2017-04-14 18:18:04 +0300

    Database created

--- webornio/store/admin.py
+++ webornio/store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Game
+from .models import Player
+from .models import Developer
+from .models import SaveGame
+from .models import Sale
+from .models import Score
 # Register your models here.
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Developer)
+admin.site.register(SaveGame)
+admin.site.register(Sale)
+admin.site.register(Score)
--- webornio/store/models.py
+++ webornio/store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Player(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Developer(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Game(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.IntegerField()
+  developer = models.ForeignKey(Developer)
+  url = models.CharField(max_length=333, default='')
+
+class Sale(models.Model):
+  game = models.ForeignKey(Game)
+  player = models.ForeignKey(Player)
+  price = models.IntegerField()
+
+class SaveGame(models.Model):
+  gameState = models.TextField()
+  player = models.ForeignKey(Player)
+  game = models.ForeignKey(Game)
+
+class Score(models.Model):
+  score = models.IntegerField()
+  player = models.ForeignKey(Player)
+  game = models.ForeignKey(Game)
