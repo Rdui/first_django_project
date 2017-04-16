4fbadc5ced0b3dbd8e609f129c7124ded79b6ae6
Tommi Nikula <tommi.nikula@student.tut.fi>
2017-04-16 15:37:21 +0300

    Context passing from views to template working

--- webornio/store/views.py
+++ webornio/store/views.py
@@ -1,8 +1,18 @@
 #-*- coding:UTF-8 -*-
 from django.shortcuts import render
+from django.template import loader
+
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hei maailma, katsot storen indeksiä.")
+
+def game(request, game_id):
+    url = "HOMO"    #get URL from database
+    template = loader.get_template('store/game.html')
+    context = {
+        'game_url': url,
+    }
+    return HttpResponse(template.render(context, request))
