433656c3e91476bebc37d76e664281e6f695949d
Juho Vähätalo <Juho Vhtalo>
2017-04-23 22:48:36 +0300

    Heroku stuff

--- webornio/webornio/settings.py
+++ webornio/webornio/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
+DATABASE_URL = 'postgres://crkglzhglewaxu:a15c170b24e2cddeb350e052598442f0e183e1bd1c783d5df6dfb5e938c87803@ec2-54-225-118-55.compute-1.amazonaws.com:5432/d7f4p3bs42v0f2'
 
 LOGIN_REDIRECT_URL="/store/profile"
 
@@ -86,13 +88,18 @@ WSGI_APPLICATION = 'webornio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
+DATABASES = {
+                'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+             }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -130,4 +137,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#Whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
